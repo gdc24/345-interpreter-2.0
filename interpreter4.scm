6a8a71832fad7df9476cc45892f44f9f334b049c
gdc24 <gdc24@case.edu>
2018-04-30 23:51:57 -0400

    added names

--- interpreter4.scm
+++ interpreter4.scm
@@ -1,3 +1,6 @@
+; Giuliana Conte gdc24
+; Taylor Smith tps45
+
 ; If you are using racket instead of scheme, uncomment these two lines, comment the (load "simpleParser.scm") and uncomment the (require "simpleParser.scm")
 ; #lang racket
 ; (require "simpleParser.scm")
